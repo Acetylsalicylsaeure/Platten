5e5bc9ca4e092bb19a0517af1acc63c2b06e720c
Acetylsalicylsaeure <110168604+Acetylsalicylsaeure@users.noreply.github.com>
2024-10-10 14:48:07 +0300

    add version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.acetylsalicylsaeure.platten"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 4
+        versionName = "initial release"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
